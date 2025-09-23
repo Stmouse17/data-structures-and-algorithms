c9ffbdf6a237d6d15f88afaf1b14235b5d0b3733
Stmouse17 <sintream@outlook.com>
2025-09-23 17:17:14 +0800

    Add files via upload

---
+++ Chapter3 Stack&Queue/homework/3.9.c
@@ -0,0 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	printf("This type of question is too intellectual, there is no need to write code to complete it\n");
+	return 0;
+}
